4162d93c9333b6ae00d34f5a8ac212fca44bc65e
SeongIl037 <kerect@naver.com>
2025-06-25 12:40:56 +0900

    api 지우기

--- Assets/02.Script/APIKeys.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class APIKeys
-{
-    public static string OPENAI_API_KEY =
-        "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";
-    public static string TYPECAST_API_KEY =
-        "__pltB4BvLfwRfrbB3YmHpXU9DgPW2Uwai3WZPrfm5kMr";
-}
